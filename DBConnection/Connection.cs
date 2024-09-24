694a603b538c8cf99d6d5379d4ff0ff5b26596ba
Nico <nico-osigus@unitybox.de>
2024-09-24 08:21:45 +0200

    FirstDBConnection

---
+++ DBConnection/Connection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DBConnection
+{
+    public class Connection : Program
+    { 
+        public bool DBConnect()
+        {
+            try
+            { 
+                string sConnectionString = @"Data Source = MyServerName; Initial Catalog = MyDbName; User ID = Admin; Password = Root";
+                SqlConnection sqlconnection = new SqlConnection(sConnectionString);
+                return true;
+                
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+         }
+    }
+}
---
+++ DBConnection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBConnection
+{
+    public class Program
+    { 
+
+        public void Main()
+        {
+            Connection oConnection = new Connection();
+            oConnection.DBConnect();
+            if (oConnection.DBConnect() == true)
+            {
+
+            }
+
+        }
+    }
+}
